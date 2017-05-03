a110c59614dc8a11881e453bbb01bdbcdc0196f3
guozaiss <492321329@qq.com>
2017-05-03 18:15:09 +0800

    d

--- JavaWebcore/src/main/java/com/example/dao/MeiZhiMapper.java
+++ JavaWebcore/src/main/java/com/example/dao/MeiZhiMapper.java
@@ -1,15 +1,29 @@
 package com.example.dao;
 
 import com.example.model.MeiZhi;
+import com.example.model.MeiZhiExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface MeiZhiMapper {
+    int countByExample(MeiZhiExample example);
+
+    int deleteByExample(MeiZhiExample example);
+
     int deleteByPrimaryKey(String id);
 
     int insert(MeiZhi record);
 
     int insertSelective(MeiZhi record);
 
+    List<MeiZhi> selectByExample(MeiZhiExample example);
+
     MeiZhi selectByPrimaryKey(String id);
+
+    int updateByExampleSelective(@Param("record") MeiZhi record, @Param("example") MeiZhiExample example);
+
+    int updateByExample(@Param("record") MeiZhi record, @Param("example") MeiZhiExample example);
 
     int updateByPrimaryKeySelective(MeiZhi record);
 
